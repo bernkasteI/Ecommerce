881173f82581635b225e92b04131899c25834a88
Maria Todorova <74039656+bernkasteI@users.noreply.github.com>
2023-09-30 21:33:38 -0500

    Duplicate directory

--- my-app2/src/redux/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'; //combineReducers expects at least one reducer
-
-import userReducer from './User/user.reducer';
-
-export default combineReducers(
-    {
-        user: userReducer
-    }
-)
